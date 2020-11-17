67871cf2be29bfa3acda3eb1530c2a3f22a94ee7
MotNosniktaw <tomwatkinson90gmail.com>
2020-11-17 12:41:54 +0000

    updated push Ticket Response Error Code to string to match return object from expo push notifications endpoint

--- src/Models/PushTicketResponse.cs
+++ src/Models/PushTicketResponse.cs
@@ -1,43 +1,41 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
 
-namespace Expo.Server.Models
-{
-    [JsonObject(MemberSerialization.OptIn)]
-    public class PushTicketResponse
-    {
-        [JsonProperty(PropertyName = "data")]
+namespace Expo.Server.Models {
+    [JsonObject (MemberSerialization.OptIn)]
+    public class PushTicketResponse {
+        [JsonProperty (PropertyName = "data")]
         public List<PushTicketStatus> PushTicketStatuses { get; set; }
 
-        [JsonProperty(PropertyName = "errors")]
+        [JsonProperty (PropertyName = "errors")]
         public List<PushTicketErrors> PushTicketErrors { get; set; }
 
     }
 
-    [JsonObject(MemberSerialization.OptIn)]
+    [JsonObject (MemberSerialization.OptIn)]
     public class PushTicketStatus {
 
-        [JsonProperty(PropertyName = "status")] //"error" | "ok",
+        [JsonProperty (PropertyName = "status")] //"error" | "ok",
         public string TicketStatus { get; set; }
 
-        [JsonProperty(PropertyName = "id")]
+        [JsonProperty (PropertyName = "id")]
         public string TicketId { get; set; }
 
-        [JsonProperty(PropertyName = "message")]
+        [JsonProperty (PropertyName = "message")]
         public string TicketMessage { get; set; }
 
-        [JsonProperty(PropertyName = "details")]
+        [JsonProperty (PropertyName = "details")]
         public object TicketDetails { get; set; }
     }
 
-    [JsonObject(MemberSerialization.OptIn)]
+    [JsonObject (MemberSerialization.OptIn)]
     public class PushTicketErrors {
-        [JsonProperty(PropertyName = "code")]
-        public int ErrorCode { get; set; }
+        [JsonProperty (PropertyName = "code")]
+        public string ErrorCode { get; set; }
 
-        [JsonProperty(PropertyName = "message")]
+        [JsonProperty (PropertyName = "message")]
         public string ErrorMessage { get; set; }
     }
 }
